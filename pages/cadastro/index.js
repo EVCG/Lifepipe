0849685df397a7508380a2fe69c22fb6b3ab75e8
lucas araujo <lucasao744@gmail.com>
2024-07-23 13:21:39 -0300

    arrumando as pasta de login e cadastro

---
+++ pages/cadastro/index.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./cadastro.module.css";
+
+export default function Cadastro() {
+  return (
+    <>
+      Cadastro
+    </>
+  );
+}
---
+++ pages/login/index.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./login.module.css";
+
+export default function Login() {
+  return (
+    <>
+      Login
+    </>
+  );
+}
