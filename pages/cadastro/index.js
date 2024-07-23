e19bfd5d1cca1ced3006e5966d762f80503ab54e
lucas araujo <lucasao744@gmail.com>
2024-07-23 15:40:13 -0300

    fazendo a tela de cadastro

--- pages/cadastro/index.js
+++ pages/cadastro/index.js
@@ -1,10 +1,38 @@
 import Image from "next/image";
 import styles from "./cadastro.module.css";
+import { useRouter } from "next/router";
 
 export default function Cadastro() {
+  const router = useRouter()
   return (
-    <>
-      Cadastro
-    </>
+    <div className={styles.ContainerCadastro}>
+      <div className={styles.ContentRegister}>
+        <h1 className={styles.TitleScreenRegister}>Crie sua conta</h1>
+        <h2 className={styles.DescriptionScreenRegister}>Preencha todos os dados abaixo, as informações serão avaliadas pela nossa equipe e em breve confirmadas via E-mail em um prazo de 48 horas.</h2>
+
+        <form className={styles.Contentform}>
+          <div className={styles.contentinputs}>
+            <input type="text" className={styles.iputsRegister} placeholder="Nome da Empresa"/>
+          </div>
+          <div className={styles.contentinputs}>
+            <input type="text" className={styles.iputsRegisterCnpj} placeholder="CNPJ"/>
+            <input type="text" className={styles.iputsRegisterContato} placeholder="Contato"/>
+          </div>
+          <div className={styles.contentinputs}>
+            <input type="text" className={styles.iputsRegister} placeholder="Endereço Completo"/>
+          </div>
+
+          <button className={styles.ButtonRegister}>Cadastrar</button>
+        </form>
+
+      </div>
+
+      <div className={styles.ContentDescription}>
+        <h1 className={styles.TitleRegister}>LifePipe</h1>
+        <h2  className={styles.SubTitleRegister}>Já possui conta?</h2>
+        <p  className={styles.DescriptionText}>Não perca tempo, faça seu login e volte a usar nossos serviços.</p>
+        <button className={styles.ButtonLoginRegister} onClick={() => router.push("/login")}>Entrar</button>
+      </div>
+    </div>
   );
 }
