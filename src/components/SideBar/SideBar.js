0095f849555c539c8e47a1f687bf0ac1dbcfb6b5
lucas araujo <lucasao744@gmail.com>
2024-07-23 22:15:49 -0300

    add footer e sideBar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,8 +5,30 @@ export default function SideBar() {
   const router = useRouter();
 
   return (
-    <div>
-      
+    <div className={styles.ContainerSideBar}>
+      <div>
+        <p className={styles.TitleButtons}>Quadros</p>
+      </div>
+      <div>
+        <p className={styles.TitleButtons}>Templates</p>
+      </div>
+      <div className={styles.LineSideBar}/>
+      <div>
+        <p className={styles.TitleButtons}>Membros</p>
+      </div>
+      <div>
+        <p className={styles.TitleButtons}>Favoritos</p>
+      </div>
+      <div>
+        <p className={styles.TitleButtons}>Notificações</p>
+      </div>
+      <div>
+        <p className={styles.TitleButtons}>Chat</p>
+      </div>
+      <div>
+        <p className={styles.TitleButtons}>Configurações</p>
+      </div>
+
     </div>
   );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,19 @@
 import styles from './home.module.css';
 import { useRouter } from 'next/router';
 import NavBar from "@/src/components/navBar/navBar";
+import SideBar from '@/src/components/SideBar/SideBar';
+import Footer from '@/src/components/footer/footer';
 export default function Home() {
   const router = useRouter();
 
   return (
     <>
       <NavBar />
-      <div>
-
+          <div className={styles.ContainerHome}>
+        <SideBar/>
+        <div>Dados</div>
       </div>
+      <Footer/>
     </>
   );
 }
