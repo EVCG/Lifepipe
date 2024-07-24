7b4b3bd433bac0fe1cd6ea33dd4bf2d6dbba6dff
lucas araujo <lucasao744@gmail.com>
2024-07-24 12:31:47 -0300

    ajustes tela sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,31 +1,45 @@
 import styles from './sideBar.module.css';
 import { useRouter } from 'next/router';
+import { FaHeart } from "react-icons/fa";
+import { IoPersonSharp } from "react-icons/io5";
+import { IoMdNotifications } from "react-icons/io";
+import { IoIosChatbubbles } from "react-icons/io";
+import { IoSettings } from "react-icons/io5";
+import { PiSquareHalfFill } from "react-icons/pi";
+
 
 export default function SideBar() {
   const router = useRouter();
 
   return (
     <div className={styles.ContainerSideBar}>
-      <div>
+      <div className={styles.contentIconSideBar}>
+      <PiSquareHalfFill />
         <p className={styles.TitleButtons}>Quadros</p>
       </div>
-      <div>
+      <div className={styles.contentIconSideBar}>
+      <PiSquareHalfFill />
         <p className={styles.TitleButtons}>Templates</p>
       </div>
       <div className={styles.LineSideBar}/>
-      <div>
+      <div className={styles.contentIconSideBar}>
+      <IoPersonSharp />
         <p className={styles.TitleButtons}>Membros</p>
       </div>
-      <div>
+      <div className={styles.contentIconSideBar}>
+      <FaHeart />
         <p className={styles.TitleButtons}>Favoritos</p>
       </div>
-      <div>
+      <div className={styles.contentIconSideBar}>
+      <IoMdNotifications />
         <p className={styles.TitleButtons}>Notificações</p>
       </div>
-      <div>
+      <div className={styles.contentIconSideBar}>
+      <IoIosChatbubbles />
         <p className={styles.TitleButtons}>Chat</p>
       </div>
-      <div>
+      <div className={styles.contentIconSideBar}>
+      <IoSettings />
         <p className={styles.TitleButtons}>Configurações</p>
       </div>
 
