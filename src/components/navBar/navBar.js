3f55cdf5c8edf5c88d13d85bbc27558df61c2b9a
lucas araujo <lucasao744@gmail.com>
2024-07-23 21:50:06 -0300

    Terminado a navbar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,17 +1,22 @@
 import styles from './navBar.module.css';
 import { useRouter } from 'next/router';
 import Image from "next/image";
-import perfil from "@/src/image/perfil.png"
+import perfil from "@/src/image/perfil.png";
+import { IoSearch } from "react-icons/io5";
+
 export default function NavBar() {
   const router = useRouter();
 
   return (
     <div className={styles.ContainerNavBar}>
-      <p>LifePipe</p>
-      <button>inicio</button>
-      <p>Tarefas e solicitações</p>
-      <input type='text'/>
-      <div> <Image src={perfil} width={50} height={50} alt='iamgem de perfil'/></div>
+      <p className={styles.LogoNavBar}>LifePipe</p>
+      <button className={styles.ButtonNavBar}>inicio</button>
+      <p className={styles.tasksNavBar}>Tarefas e solicitações</p>
+      <div className={styles.searchContainer}>
+        <IoSearch className={styles.searchIcon} />
+        <input type='text' className={styles.inputNavBar} placeholder="Pesquisar..." />
+      </div>
+      <div className={styles.Line}> <Image src={perfil} width={55} height={55} alt='iamgem de perfil' /></div>
     </div>
   );
 }
