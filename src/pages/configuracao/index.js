4845bf9893b9e67bd97e81cc35b606c3e1f314e8
Elisson Victor <138872302+EVCG@users.noreply.github.com>
2024-07-25 15:25:49 -0300

    componente configuração inicio

---
+++ src/pages/configuracao/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from'./configuracao.module.css';
+import NavBar from '@/src/components/navBar/navBar';
+import Footer from '@/src/components/footer/footer';
+
+
+export default function TarefasSolicitacao() {
+
+  return (
+    <>
+    <NavBar/>
+
+    <div>
+    
+    </div>
+
+    <Footer/>
+    </>
+  );
+}
