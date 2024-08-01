a7051c21ad9d74fd6bce845110c18c15e078a1f3
Elisson Victor <138872302+EVCG@users.noreply.github.com>
2024-08-01 18:30:13 -0300

    kanban em andamento

--- src/pages/kanban/ColunaCard.js
+++ src/pages/kanban/ColunaCard.js
@@ -1,17 +1,27 @@
+import styles from './kanban.module.css'
+
 export default function ColunaCard() {
     return (
-        <>
-            <Coluna titulo='Analise de viabilidade' />
-        </>
+        <div className={styles.colunacards}>
+            <Coluna titulo='Análise de viabilidade' classe='coluna-viabilidade' />
+            <Coluna titulo='Elaboração de Custos' classe='coluna-custos' />
+            <Coluna titulo='Aguardando o Pregão' classe='coluna-pregão' />
+            <Coluna titulo='Pregões Encerrados' classe='coluna-encerrados' />
+            <Coluna titulo='Pregões ganhos' classe='coluna-ganhos' />
+
+        </div>
     )
 }
 
 function Coluna(props) {
     console.log(props)
-    const titulo = "Titulo do card"
+
     return (
-        <div>
-            <h1>{props.titulo}</h1>
+        <div className={styles.conteudo}>
+            <div className={`${styles.coluna} ${styles[props.classe]}`}>
+                <h1>{props.titulo}</h1>
+            </div>
+            <p className={styles.espacoCards}></p>
         </div>
     )
 }
